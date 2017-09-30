a9d49439456d00052212d142f5c4bd295cc58cf8
Bigotry <3162875@qq.com>
2017-09-30 12:31:58 +0800

    解决跨域问题

--- app/api/controller/ApiBase.php
+++ app/api/controller/ApiBase.php
@@ -19,6 +19,8 @@ class ApiBase extends ControllerBase
         
         parent::__construct();
         
+        header("Access-Control-Allow-Origin: *");
+        
         self::$apiBaseLogic = get_sington_object('apiBaseLogic', LogicApiBase::class);
         
         IS_POST && self::$apiBaseLogic->checkParam($this->param);
