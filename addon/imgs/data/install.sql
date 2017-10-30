c397b42ab486ac5aa00a7f5d465d9a47b64b68b6
Bigotry <3162875@qq.com>
2017-10-30 20:38:17 +0800

    新增小图标选择插件 Icon， 文件上传插件换为 Jack H5版 ，最新数据库备份更新

--- addon/imgs/data/install.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `ob_hook` (`name`, `describe`, `addon_list`, `status`, `update_time`, `create_time`) VALUES ('Imgs', '多图上传钩子', 'Imgs', '1', '0', '0');
-
-INSERT INTO `ob_addon` (`name`, `title`, `describe`, `config`,  `author`, `version`, `status`, `create_time` , `update_time`)  VALUES ('Imgs', '多图上传', '多图上传插件', '', 'Bigotry', '1.0', '1', '0', '0');
--- addon/imgs/data/uninstall.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM ob_hook WHERE `name` = 'Imgs';
-
-DELETE FROM ob_addon WHERE `name` = 'Imgs';
