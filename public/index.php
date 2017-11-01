0a9833f349668f63f90fe1c9d06892e6b6b80fca
Bigotry <3162875@qq.com>
2017-11-01 18:27:41 +0800

    执行日志记录模块，安装引导模块

--- public/index.php
+++ public/index.php
@@ -3,5 +3,8 @@
 // 初始化系统
 require  './init.php';
 
+// 检测是否安装
+file_exists(APP_PATH . 'database.php') ?  define('BIND_MODULE', 'index') : define('BIND_MODULE', 'install');
+
 // 加载框架引导文件
 require FRAMEWORK_START_PATH;
--- public/init.php
+++ public/init.php
@@ -1,5 +1,8 @@
 <?php
 
+// PHP版本验证需要大于7.0.0
+if(version_compare(PHP_VERSION, '7.0.0', '<')) : die('OneBase Require PHP > 7.0.0 !'); endif;
+
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../app/');
 
