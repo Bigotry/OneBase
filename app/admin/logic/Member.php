fbc29b77aeb619b00eae4348dfdfbf2d55869326
Bigotry <3162875@qq.com>
2017-11-22 18:46:57 +0800

    修复后台 password_confirm 字段不存在问题

--- app/admin/logic/Member.php
+++ app/admin/logic/Member.php
@@ -165,6 +165,8 @@ class Member extends AdminBase
         $data['leader_id'] = MEMBER_ID;
         $data['is_inside'] = DATA_NORMAL;
         
+        if (isset($data['password_confirm'])) : unset($data['password_confirm']); endif;
+        
         $result = self::$memberModel->addInfo($data);
         
         $result && action_log('新增', '新增会员，username：' . $data['username']);
