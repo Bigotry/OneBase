79f288485480b6d2ccca994840afc418abf98925
Bigotry <3162875@qq.com>
2017-12-07 15:19:59 +0800

    PJAX模式下表单数据回填问题修复

--- public/static/admin/js/pjax_init.js
+++ public/static/admin/js/pjax_init.js
@@ -1,23 +1,125 @@
 $.pjax.defaults.timeout = 10000;
 $.pjax.defaults.maxCacheLength = 0;
 
-if(pjax_mode == 1)
-{
-    $(document).pjax('a', '.content');
-
-    $(document).on('submit', 'form', function(event) {
-
-        $.pjax.submit(event, '.content');
-    });
-}
+pjax_mode == 1 && $(document).pjax('a[target!=_blank]', '.content');
 
 $(document).on('pjax:send', function() {
-
+    
     $('.fakeloader').show();
 });
 
 $(document).on('pjax:complete', function() {
-
-    $('.fakeloader').hide();
-});
     
+    $('.fakeloader').hide();
+
+    /**
+     * PJAX模式重写get请求提交处理
+     */
+    $('.ajax-get').click(function(){
+        
+        var target;
+        
+        if ( $(this).hasClass('confirm') ) {
+            
+            if(!confirm('确认要执行该操作吗?')){
+                
+                return false;
+            }
+        }
+        
+        if ( (target = $(this).attr('href')) || (target = $(this).attr('url')) ) {
+            
+            $.get(target).success(function(data){  obalertp(data);  });
+        }
+        
+        return false;
+    });
+
+    /**
+     * PJAX模式重写表单POST提交处理
+     */
+    $('.ajax-post').click(function(){
+
+        var target,query,form;
+
+        var target_form = $(this).attr('target-form');
+
+        var that = this;
+
+        var nead_confirm=false;
+
+        if( ($(this).attr('type')=='submit') || (target = $(this).attr('href')) || (target = $(this).attr('url')) ){
+
+            form = $('.'+target_form);
+
+            if ($(this).attr('hide-data') === 'true'){//无数据时也可以使用的功能
+                form = $('.hide-data');
+                query = form.serialize();
+            }else if (form.get(0)==undefined){
+                return false;
+            }else if ( form.get(0).nodeName=='FORM' ){
+
+                if ( $(this).hasClass('confirm') ) {
+
+                    if(!confirm('确认要执行该操作吗?')){
+
+                        return false;
+                    }
+                }
+                if($(this).attr('url') !== undefined){
+                        target = $(this).attr('url');
+                }else{
+                        target = form.get(0).action;
+                }
+                query = form.serialize();
+            }else if( form.get(0).nodeName=='INPUT' || form.get(0).nodeName=='SELECT' || form.get(0).nodeName=='TEXTAREA') {
+
+                form.each(function(k,v){
+                    if(v.type=='checkbox' && v.checked==true){
+                        nead_confirm = true;
+                    }
+                })
+
+                if ( nead_confirm && $(this).hasClass('confirm') ) {
+                    if(!confirm('确认要执行该操作吗?')){
+                        return false;
+                    }
+                }
+
+                query = form.serialize();
+            }else{
+
+                if ( $(this).hasClass('confirm') ) {
+                    if(!confirm('确认要执行该操作吗?')){
+                        return false;
+                    }
+                }
+                query = form.find('input,select,textarea').serialize();
+            }
+
+            var is_ladda_button =  $(that).hasClass('ladda-button');
+
+            is_ladda_button ? button.start('.ladda-button') : $(that).addClass('disabled').attr('autocomplete','off').prop('disabled',true);
+
+            $.post(target,query).success(function(data){
+
+                obalertp(data);
+                
+                is_ladda_button ? button.stop('.ladda-button') : $(that).removeClass('disabled').prop('disabled',false);
+            });
+        }
+
+        return false;
+    });
+
+});
+
+/**
+ * PJAX模式重写跳转处理
+ */
+var obalertp = function (data) {
+    	
+    data.code ? toast.success(data.msg) : toast.error(data.msg);
+
+    data.url && $.pjax({url: data.url,container: '.content'});
+};
