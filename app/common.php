3042438d3183686ee0e3f46431c3db6f051fb315
Bigotry <3162875@qq.com>
2017-12-09 16:15:17 +0800

    加入获取区间日期函数

--- app/common.php
+++ app/common.php
@@ -684,6 +684,29 @@ function get_os()
 }
 
 /**
+ * 获取指定日期段内每一天的日期
+ * @param Date $startdate 开始日期
+ * @param Date $enddate  结束日期
+ * @return Array
+ */
+function get_date_from_range($startdate, $enddate)
+{
+    
+  $stimestamp = strtotime($startdate);
+  $etimestamp = strtotime($enddate);
+  
+  // 计算日期段内有多少天
+  $days = ($etimestamp-$stimestamp)/86400+1;
+  
+  // 保存每天日期
+  $date = [];
+  
+  for($i=0; $i<$days; $i++) : $date[] = date('Y-m-d', $stimestamp+(86400*$i)); endfor;
+  
+  return $date;
+}
+
+/**
  * 获得浏览器
  */
 function browser_info()
