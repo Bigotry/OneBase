ab54993b006ba420a3a867ccfa450b50c1d508c5
Bigotry <3162875@qq.com>
2017-12-11 17:35:19 +0800

    PHP所需版本调整为5.6+

--- public/init.php
+++ public/init.php
@@ -1,7 +1,7 @@
 <?php
 
-// PHP版本验证需要大于7.0.0
-if(version_compare(PHP_VERSION, '7.0.0', '<')) : die('OneBase Require PHP > 7.0.0 !'); endif;
+// PHP版本验证需要大于5.6.0
+if(version_compare(PHP_VERSION, '5.6.0', '<')) : die('OneBase Require PHP > 5.6.0 !'); endif;
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../app/');
