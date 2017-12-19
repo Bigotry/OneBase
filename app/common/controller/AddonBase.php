fb64509a8662e13470189a8c240eb8e54f9f1ea9
Bigotry <3162875@qq.com>
2017-12-19 11:35:32 +0800

    重写插件模块fetch方法，与其他模块模板渲染相同使用，支持__STATIC__默认指向插件static目录

--- app/common/controller/AddonBase.php
+++ app/common/controller/AddonBase.php
@@ -12,25 +12,29 @@ class AddonBase extends ControllerBase
 {
     
     /**
-     * 插件模板渲染
+     * 重写加载模板输出
+     * @access protected
+     * @param string $template 模板文件名
+     * @param array  $vars     模板输出变量
+     * @param array  $replace  模板替换
+     * @param array  $config   模板参数
+     * @return mixed
      */
-    public function addonTemplate($template_name = '')
+    protected function fetch($template = '', $vars = [], $replace = [], $config = [])
     {
         
         $class = get_class($this);
         
         $addon_name = strtolower(substr($class, DATA_NORMAL + strrpos($class, SYS_DS_CONS)));
         
-        $addon_path = PATH_ADDON . $addon_name . DS;
+        $view_path = PATH_ADDON . $addon_name . DS . LAYER_VIEW_NAME . DS;
         
-        $view_path = $addon_path . LAYER_VIEW_NAME . DS;
+        $static_path = SYS_DS_PROS .SYS_ADDON_DIR_NAME. SYS_DS_PROS . $addon_name . SYS_DS_PROS . SYS_STATIC_DIR_NAME;
         
-        $static_path =  SYS_DS_PROS .SYS_ADDON_DIR_NAME. SYS_DS_PROS . $addon_name . SYS_DS_PROS . SYS_STATIC_DIR_NAME . SYS_DS_PROS;
-        
-        $this->assign('static_path', $static_path);
+        $replace_default['__STATIC__'] = $static_path; 
         
         $this->view->engine(['view_path' => $view_path]);
         
-        echo $this->fetch($template_name);
+        echo $this->view->fetch($template, $vars, array_merge($replace_default, $replace), $config);
     }
 }
