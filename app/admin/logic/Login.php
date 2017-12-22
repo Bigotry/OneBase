e1283a37b9bc0c77a2d5256f901af9c7b094db45
Bigotry <3162875@qq.com>
2017-12-22 22:40:32 +0800

    后台业务逻辑层登录优化

--- app/admin/logic/Login.php
+++ app/admin/logic/Login.php
@@ -16,36 +16,22 @@ class Login extends AdminBase
      */
     public function loginHandle($username = '', $password = '', $verify = '')
     {
-
-        if (empty($username) || empty($password)) : return [RESULT_ERROR, '账号或密码不能为空']; endif;
         
-        if (empty($verify)) : return [RESULT_ERROR, '验证码不能为空']; endif;
-       
-        if (!captcha_check($verify)) : return [RESULT_ERROR, '验证码输入错误']; endif;
+        $member = $this->validateMember->checkLoginData(compact('username','password','verify'));
         
-        $member = $this->logicMember->getMemberInfo(['username' => $username]);
+        if (false === $member) : return [RESULT_ERROR, $this->validateMember->getError()]; endif;
+            
+        $this->logicMember->setMemberValue(['id' => $member['id']], TIME_UT_NAME, TIME_NOW);
 
-        if (empty($member)) : return [RESULT_ERROR, '用户不存在']; endif;
-        
-        // 验证用户密码
-        if (data_md5_key($password) === $member['password']) {
-            
-            $this->logicMember->setMemberValue(['id' => $member['id']], TIME_UT_NAME, TIME_NOW);
-            
-            $auth = ['member_id' => $member['id'], TIME_UT_NAME => TIME_NOW];
-            
-            session('member_info', $member);
-            session('member_auth', $auth);
-            session('member_auth_sign', data_auth_sign($auth));
+        $auth = ['member_id' => $member['id'], TIME_UT_NAME => TIME_NOW];
 
-            action_log('登录', '登录操作，username：'. $username);
-            
-            return [RESULT_SUCCESS, '登录成功', url('index/index')];
+        session('member_info', $member);
+        session('member_auth', $auth);
+        session('member_auth_sign', data_auth_sign($auth));
 
-        } else {
-            
-            return [RESULT_ERROR, '密码输入错误'];
-        }
+        action_log('登录', '登录操作，username：'. $username);
+
+        return [RESULT_SUCCESS, '登录成功', url('index/index')];
     }
     
     /**
--- app/admin/validate/Member.php
+++ app/admin/validate/Member.php
@@ -5,6 +5,8 @@
 
 namespace app\admin\validate;
 
+use app\admin\logic\Member as LogicMember;
+
 /**
  * 会员验证器
  */
@@ -38,4 +40,22 @@ class Member extends AdminBase
         'add'  =>  ['username','password','email'],
     ];
     
+    // 扩展验证规则 验证会员登录信息
+    public function checkLoginData($data)
+    {
+        
+        if(empty($data['username']))                                : $this->error = '账号不能为空';    return false; endif;
+        if(empty($data['password']))                                : $this->error = '密码不能为空';    return false; endif;
+        if(empty($data['verify']))                                  : $this->error = '验证码不能为空';  return false; endif;
+        if(!captcha_check($data['verify']))                         : $this->error = '验证码输入错误';  return false; endif;
+        
+        $memberLogic = get_sington_object('memberLogic', LogicMember::class);
+        
+        $member = $memberLogic->getMemberInfo(['username' => $data['username']]);
+
+        if(empty($member))                                          : $this->error = '用户账号不存在';  return false; endif;
+        if(data_md5_key($data['password']) != $member['password'])  : $this->error = '密码输入错误';    return false; endif;
+        
+        return $member;
+    }
 }
