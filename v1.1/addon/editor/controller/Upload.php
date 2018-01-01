87adaed16b100fb1876164cc9d183cc536cdbcaf
Bigotry <3162875@qq.com>
2018-01-01 18:21:55 +0800

    OneBase1.1

---
+++ v1.1/addon/editor/controller/Upload.php
@@ -0,0 +1,33 @@
+<?php
+// +---------------------------------------------------------------------+
+// | OneBase    | [ WE CAN DO IT JUST THINK ]                            |
+// +---------------------------------------------------------------------+
+// | Licensed   | http://www.apache.org/licenses/LICENSE-2.0 )           |
+// +---------------------------------------------------------------------+
+// | Author     | Bigotry <3162875@qq.com>                               |
+// +---------------------------------------------------------------------+
+// | Repository | https://gitee.com/Bigotry/OneBase                      |
+// +---------------------------------------------------------------------+
+
+namespace addon\editor\controller;
+
+use app\common\controller\AddonBase;
+use addon\editor\logic\Upload as LogicUpload;
+
+/**
+ * 编辑器插件上传控制器
+ */
+class Upload extends AddonBase
+{
+
+    /**
+     * 图片上传
+     */
+    public function pictureUpload()
+    {
+        
+        $result = (new LogicUpload())->pictureUpload();
+        
+        return throw_response_exception($result);
+    }
+}
