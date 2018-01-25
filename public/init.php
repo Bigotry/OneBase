3a30508517be28e5b69442b7f488b5c35115f43a
Bigotry <3162875@qq.com>
2018-01-25 16:33:45 +0800

    代码优化

--- public/init.php
+++ public/init.php
@@ -10,7 +10,10 @@
 // +---------------------------------------------------------------------+
 
 // PHP版本验证需要大于5.6.0
-if(version_compare(PHP_VERSION, '5.6.0', '<')) : die('OneBase Require PHP > 5.6.0 !'); endif;
+if(version_compare(PHP_VERSION, '5.6.0', '<')) {
+    
+    die('OneBase Require PHP > 5.6.0 !');
+}
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../app/');
