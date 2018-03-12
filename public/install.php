cd76fb73e77b0e0d6bc823102520811f32a17d01
Bigotry <3162875@qq.com>
2018-03-12 17:23:40 +0800

    前端静态资源目录结构调整

--- public/install.php
+++ public/install.php
@@ -18,12 +18,8 @@ if (version_compare(PHP_VERSION, '5.6.0', '<')) {
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../app/');
 
-// 检测是否安装
-if (!file_exists(APP_PATH . 'database.php')) {
-    
-    header("location:./install.php");
-    exit;
-}
+// 绑定安装模块
+define('BIND_MODULE', 'install');
 
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
