d6c3a93e930454596ce64e54e177548592839491
Bigotry <3162875@qq.com>
2018-03-12 17:25:51 +0800

    前端静态资源目录结构调整

--- app/index/config.php
+++ app/index/config.php
@@ -11,18 +11,7 @@
 
 // 前端配置文件
 
-$frontend_theme = config('frontend_theme');
-
-$module         = request()->module();
-
-empty(STATIC_DOMAIN) ? $static = [] :  $static['__STATIC__'] = $static_domain . SYS_DS_PROS . SYS_STATIC_DIR_NAME;
-
-$view_path = ROOT_PATH . SYS_APP_NAMESPACE . SYS_DS_PROS . $module. SYS_DS_PROS . LAYER_VIEW_NAME . SYS_DS_PROS . $frontend_theme . SYS_DS_PROS;
-
 return [
     
-    // 视图输出字符串内容替换
-    'view_replace_str' => $static,
-    
-    'template' => ['view_path' => $view_path, 'layout_on' =>  true, 'layout_name' => 'layout'],
+    'template' => ['layout_on' =>  true, 'layout_name' => 'layout'],
 ];
