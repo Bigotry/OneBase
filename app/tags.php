f81761fe604c61d486a943c6d49fcb7d909ead28
Bigotry <3162875@qq.com>
2018-03-12 17:27:14 +0800

    前端静态资源目录结构调整，安装模块重构

--- app/tags.php
+++ app/tags.php
@@ -13,10 +13,6 @@
 define('BEHAVIOR_PATH', 'app\\common\\behavior\\');
 
 $data = [
-    // 应用初始化
-    'app_init'     => [],
-    // 应用开始
-    'app_begin'    => [],
     // 模块初始化
     'module_init'  => [],
     // 操作开始执行
@@ -25,12 +21,10 @@ $data = [
     'view_filter'  => [],
     // 日志写入
     'log_write'    => [],
-    // 应用结束
-    'app_end'      => [],
 ];
 
 // 若不为安装流程则初始化系统行为
-if (BIND_MODULE != 'install') {
+if (!defined('BIND_MODULE')) {
     
     $data['app_init']   = [BEHAVIOR_PATH . 'InitBase', BEHAVIOR_PATH . 'InitHook'];
     $data['app_begin']  = [BEHAVIOR_PATH . 'AppBegin'];
