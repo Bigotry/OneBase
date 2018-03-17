01d32b289db6d301333e4433e2ee97316e38365f
Bigotry <3162875@qq.com>
2018-03-17 14:03:29 +0800

    修复添加会员找不到字段问题

--- app/common/model/ModelBase.php
+++ app/common/model/ModelBase.php
@@ -46,7 +46,7 @@ class ModelBase extends Model
         
         if (empty($data[$pk])) {
             
-            $return_data = $this->save($data, $where, $sequence);
+            $return_data = $this->allowField(true)->save($data, $where, $sequence);
             
         } else {
             
