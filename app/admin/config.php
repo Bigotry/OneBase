979dd2ca3b90c31fb8adf4cfbcc1574f981a9bf3
Bigotry <3162875@qq.com>
2018-03-19 14:31:18 +0800

    云存储测试调整配置信息

--- app/admin/config.php
+++ app/admin/config.php
@@ -9,7 +9,7 @@
 // | Repository | https://gitee.com/Bigotry/OneBase                      |
 // +---------------------------------------------------------------------+
 
-empty(STATIC_DOMAIN) ? $static = [] :  $static['__STATIC__'] = $static_domain . SYS_DS_PROS . SYS_STATIC_DIR_NAME;
+empty(STATIC_DOMAIN) ? $static = [] :  $static['__STATIC__'] = STATIC_DOMAIN . SYS_DS_PROS . SYS_STATIC_DIR_NAME;
 
 //配置文件
 return [
