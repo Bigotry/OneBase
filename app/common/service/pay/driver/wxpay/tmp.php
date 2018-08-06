4679d3158ea588d69b05828adab88e08cd0d2c41
coolcity <44750464@qq.com>
2018-08-06 15:41:46 +0800

    微信支付，扫码支付tmp.php模板中的路径修复
    Signed-off-by: coolcity <44750464@qq.com>

--- app/common/service/pay/driver/wxpay/tmp.php
+++ app/common/service/pay/driver/wxpay/tmp.php
@@ -1,5 +1,5 @@
-<script src="__STATIC__/module/common/pay/wxpay/jquery-2.0.3.min.js"></script>
-<script src="__STATIC__/module/common/pay/wxpay/qrcode.js"></script>
+<script src="/static/module/common/pay/wxpay/jquery-2.0.3.min.js"></script>
+<script src="/static/module/common/pay/wxpay/qrcode.js"></script>
 
 <div id="wx_pay_qrcode" style="height: 180px; width: 180px; margin:200 auto; text-align: center;"></div>
 
