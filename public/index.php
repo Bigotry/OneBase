b7abef93410cf5fbf09dc6d6f6f339212e12de5a
coolcity <44750464@qq.com>
2018-08-09 17:18:02 +0800

    修复，api跨域请求，自定义header头 Signed-off-by: coolcity <44750464@qq.com>

--- public/index.php
+++ public/index.php
@@ -15,6 +15,16 @@ if (version_compare(PHP_VERSION, '5.6.0', '<')) {
     die('OneBase Require PHP > 5.6.0 !');
 }
 
+// 设置跨域请求头
+header("Access-Control-Allow-Origin: *");
+// 如果需要自定义请求头时，直接把请求头添加到下面的参数里面即可
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+// 前端VUE框架自定义Header头请求API时，会发起一次预请求，thinkphp默认是拒绝OPTIONS类型的请求的
+// 判断是否是OPTIONS请求，是的话直接exit结束，直接让浏览器返回200
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+	exit;
+}
+
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../app/');
 
