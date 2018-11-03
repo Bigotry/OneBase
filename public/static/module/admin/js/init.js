a20f721509e7eda0dd836b6c37f9c71cddf4c77c
Bigotry <3162875@qq.com>
2018-11-03 16:14:03 +0800

    优化：检测JS文件未被加载才执行动态加载

--- public/static/module/admin/js/init.js
+++ public/static/module/admin/js/init.js
@@ -65,21 +65,24 @@
         }
     };
     
+    var url_arr = new Array();
+    
     /* 批量加载JS脚本文件 */
     ob.loadMultiScripts = function(arr) {
 
         var _arr = $.map(arr, function(scr) {
 
-            return $.getScript(scr);
+            var index = $.inArray(scr,url_arr);
+            
+            if(index <= 0) {
+                return $.getScript(scr, function() {
+
+                  url_arr.push(scr);
+                });
+            }
         });
 
-        _arr.push($.Deferred(function(deferred){
-
-            $(deferred.resolve);
-
-        }));
-
         return $.when.apply($, _arr);
-    }
+    };
 
 })(jQuery);
