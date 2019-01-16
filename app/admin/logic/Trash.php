af3dd916836c89e026957b2f6468b2f15dfd2da6
mz <54600488@qq.com>
2019-01-16 09:49:37 +0800

    回收站列表/删除/恢复的自检表索引修改

--- app/admin/logic/Trash.php
+++ app/admin/logic/Trash.php
@@ -29,7 +29,7 @@ class Trash extends AdminBase
         foreach ($trash_config as $k => $v) {
             $temp = [];[$v];
             $m = LAYER_MODEL_NAME   . $k;
-            $key = $this->$m->getPk();  //获取数据表主键
+            $key = $this->$m->getPk();
             $temp['name']           = $k;
             $temp['model_path']     = $this->$m->class;
             $temp['number']         = $this->$m->stat([DATA_STATUS_NAME => DATA_DELETE],'count',$key);
@@ -49,13 +49,22 @@ class Trash extends AdminBase
         $trash_config = parse_config_array('trash_config');
         
         $dynamic_field = $trash_config[$model_name];
-        
-        $field = 'id,' . TIME_CT_NAME . ','.TIME_UT_NAME.',' . $dynamic_field;
-        
+
         $m = LAYER_MODEL_NAME . $model_name;
-        
-        $list = $this->$m->getList([DATA_STATUS_NAME => DATA_DELETE], $field, 'id desc');
-        
+
+        $key = $this->$m->getPk();
+
+        $field = $key . ',' . TIME_CT_NAME . ','.TIME_UT_NAME.',' . $dynamic_field;
+
+        $list = $this->$m->getList([DATA_STATUS_NAME => DATA_DELETE], $field, $key . ' desc');
+
+        foreach($list as $k=>$v){
+
+            $list[$k]['id'] = $v[$key];
+            
+            unset($list[$k][$key]);
+        }
+
         return compact('list', 'dynamic_field', 'model_name');
     }
     
@@ -65,13 +74,15 @@ class Trash extends AdminBase
     public function trashDataDel($model_name = '', $id = 0)
     {
         
-        $where = empty($id) ? [DATA_STATUS_NAME => DATA_DELETE] : ['id' => $id];
-        
         $m = LAYER_MODEL_NAME . $model_name;
+
+        $key = $this->$m->getPk();
+
+        $where = empty($id) ? [DATA_STATUS_NAME => DATA_DELETE] : [$key => $id];
         
         $result = $this->$m->deleteInfo($where, true);
         
-        $result && action_log('删除', '删除回收站数据，model_name：' . $model_name .'，id' . $id);
+        $result && action_log('删除', '删除回收站数据，model_name：' . $model_name .'，'. $key . $id);
         
         return $result ? [RESULT_SUCCESS, '删除成功'] : [RESULT_ERROR, '删除失败'];
     }
@@ -81,14 +92,15 @@ class Trash extends AdminBase
      */
     public function restoreData($model_name = '', $id = 0)
     {
-        
-        $where = empty($id) ? [DATA_STATUS_NAME => ['neq', DATA_NORMAL]] : ['id' => $id];
-        
         $m = LAYER_MODEL_NAME . $model_name;
+        
+        $key = $this->$m->getPk();
+
+        $where = empty($id) ? [DATA_STATUS_NAME => ['neq', DATA_NORMAL]] : [$key => $id];
         
         $result = $this->$m->setFieldValue($where, DATA_STATUS_NAME, DATA_NORMAL);
         
-        $result && action_log('恢复', '恢复回收站数据，model_name：' . $model_name .'，id' . $id);
+        $result && action_log('恢复', '恢复回收站数据，model_name：' . $model_name .'，'. $key . $id);
         
         return $result ? [RESULT_SUCCESS, '数据恢复成功'] : [RESULT_ERROR, '恢复失败'];
     }
